93911cf4834cc60dfcea2172993dc0eab95f3304
laviedegeorge <laviedegeorge1292@gmail.com>
2020-09-09 12:44:24 +0100

    build static UI responsive page

--- dist/ui.59cf47a3.js
+++ dist/ui.59cf47a3.js
@@ -117,83 +117,35 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
+})({"js/ui.js":[function(require,module,exports) {
+/* ------------------ FOR MOBILE TOGGLE BUTTON-------------- */
+var mainD = document.getElementById('main-details');
+var toggleBtn = document.querySelectorAll('.toggle');
+toggleBtn.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    if (e.target === btn) {
+      mainD.classList.add('side-nav');
+    } else {
+      mainD.classList.remove('side-nav');
     }
+  });
+});
+var mainWeatherInfo = document.getElementById('main_weather-info');
+mainWeatherInfo.addEventListener('click', function (e) {
+  if (e.target === mainWeatherInfo) {
+    mainD.classList.remove('side-nav');
   }
+});
+/* MORE WEATHER DETAILS */
 
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/main.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./styles/main.css");
-},{"./styles/main.css":"styles/main.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var moreInfoCont = document.getElementById('more_info_cont');
+var pushUpBtn = document.getElementById('push_up');
+pushUpBtn.addEventListener('click', function () {
+  moreInfoCont.classList.toggle('expand');
+  pushUpBtn.classList.toggle('rotate');
+  console.log('clicked');
+});
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -397,5 +349,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/ui.js"], null)
+//# sourceMappingURL=/ui.59cf47a3.js.map
